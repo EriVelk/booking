c133f349d687fd2c741cc7a9574ca93451789350
EriVelk <erve@live.com.mx>
2022-05-09 12:10:01 -0500

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+import Navbar from '../../components/navbar/Navbar'
+import "./home.css"
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Header/>
+    </div>
+  )
+}
+
+export default Home
